e617c739b671863698b65f463512facbb885f442
Kasper Tvede <kasper@webmasteren.eu>
2016-04-18 22:58:48 +0200

    made it possible to serialize a model with a objectid (given its the only one), using json.net

--- commonSenseASP/mongoDB/BaseModel.cs
+++ commonSenseASP/mongoDB/BaseModel.cs
@@ -1,13 +1,15 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace commonSenseASP.mongoDB
-{
-    public abstract class BaseModel
-    {
+namespace commonSenseASP.mongoDB {
+    [BsonIgnoreExtraElements]
+    public abstract class BaseModel {
+        [JsonConverter(typeof(ObjectIdConverter))]
         public ObjectId id { get; set; }
     }
 }
---
+++ commonSenseASP/mongoDB/ObjectIdConverter.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace commonSenseASP.mongoDB {
+    public class ObjectIdConverter : JsonConverter {
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            serializer.Serialize(writer, value.ToString());
+
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+            return ObjectId.Parse(reader.Value.ToString());
+        }
+
+        public override bool CanConvert(Type objectType) {
+            return typeof(ObjectId).IsAssignableFrom(objectType);
+            //return true;
+        }
+
+
+    }
+}
