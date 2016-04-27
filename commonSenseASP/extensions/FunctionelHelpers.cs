e95a7c7979bf32f89b979153e2e873342c9b7594
Kasper Tvede <kasper@webmasteren.eu>
2016-04-27 22:22:49 +0200

    renamed map to Map, and added comments to other functions.

--- commonSenseASP/extensions/FunctionelHelpers.cs
+++ commonSenseASP/extensions/FunctionelHelpers.cs
@@ -12,26 +12,44 @@ public static class FunctionalHelpers {
     /// <param name="input">the input list</param>
     /// <param name="generator">the generator / converter </param>
     /// <returns>the resulting collection</returns>
-    public static List<U> map<U, T>(this IEnumerable<T> input, Func<T, U> generator) {
+    public static List<U> Map<U, T>(this IEnumerable<T> input, Func<T, U> generator) {
         List<U> result = new List<U>(input.Count());
         foreach (var item in input) {
             result.Add(generator(item));
         }
         return result;
     }
-
+    /// <summary>
+    /// Performs and action over a IEnumerable (given an action.) 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="onEach"></param>
     public static void Foreach<T>(this IEnumerable<T> list, Action<T> onEach) {
         foreach (var item in list) {
             onEach(item);
         }
     }
 
+    /// <summary>
+    /// Performs and action over a IEnumerable (given a function) ignoring the result of a function
+    /// this is convenince function, if the result should be used, consider using map.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="list"></param>
+    /// <param name="onEach"></param>
     public static void Foreach<T, U>(this IEnumerable<T> list, Func<T, U> onEach) {
         foreach (var item in list) {
             onEach(item);
         }
     }
-
+    /// <summary>
+    /// Performs the given action each time starting from 0 upto the value.
+    /// Eg. replace a for loop over the variable. 
+    /// </summary>
+    /// <param name="times"></param>
+    /// <param name="callback"></param>
     public static void PerformEachTime(this int times, Action<int> callback) {
         for (int i = 0; i < times; i++) {
             callback(i);
@@ -51,7 +69,12 @@ public static class FunctionalHelpers {
         }
         return times - 1;
     }
-
+    /// <summary>
+    /// Performs an action if a value is true.
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="ifTrue"></param>
+    /// <returns></returns>
     public static bool DoOnTrue(this bool val, Action ifTrue) {
         if (val) {
             ifTrue();
@@ -59,6 +82,12 @@ public static class FunctionalHelpers {
         return val;
     }
 
+    /// <summary>
+    /// Flatterns a list (in i a  list) to a list by moving all elements in the depth into the same list. 
+    /// </summary>
+    /// <typeparam name="U"></typeparam>
+    /// <param name="collection"></param>
+    /// <returns></returns>
     public static List<U> flattern<U>(this IEnumerable<IEnumerable<U>> collection) {
         var result = new List<U>(collection.Count());
         foreach (var item in collection) {
