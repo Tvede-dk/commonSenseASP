5b0f906539b4ec1c91befc49096184ce4e1abc88
Kasper Tvede <kasper@webmasteren.eu>
2016-05-09 23:51:09 +0200

    fixed a potential error.

--- commonSenseASP/mongoDB/Service/TextSearchableBaseService.cs
+++ commonSenseASP/mongoDB/Service/TextSearchableBaseService.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 namespace commonSenseASP.mongoDB.Service {
     public abstract class TextSearchableBaseService<T> : BaseService<T> where T : BaseModel {
         public Task<List<T>> GetPaginatedTextSearch(string textSearch, int page, int limit) {
-            var filter = GetTextSearchFilter(textSearch);
+            var filter = GetTextSearchFilter(textSearch ?? "");
             return dataCollection.Find(filter).Skip(calculateCurrentPage(page, limit)).Limit(limit).ToListAsync();
         }
 
         public Task<long> GetPagniatedTextSearchCount(string textSearch, int page, int limit) {
-            var filter = GetTextSearchFilter(textSearch);
+            var filter = GetTextSearchFilter(textSearch ?? "");
             return dataCollection.Find(filter).CountAsync();
         }
 
